585d92b71eabf13582519fcc20e8dbf505463bd0
Anurag Kumar <anuragkr898@gmail.com>
2023-04-30 20:34:44 +0530

    added cart-context which keeps track of total items, added toggle cart-icon, functionality for add to cart button

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,19 @@
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
-import './cart-icon.styles.scss'
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import "./cart-icon.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
-  return (
-    <div className='cart-icon-container'>
-        <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
-    </div>
-  )
-}
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-export default CartIcon
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{cartCount}</span>
+    </div>
+  );
+};
+
+export default CartIcon;
