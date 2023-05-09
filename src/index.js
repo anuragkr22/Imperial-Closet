ee24b5e4148521a3282988c6583464335b21cdaf
Anurag Kumar <anuragkr898@gmail.com>
2023-05-09 21:51:26 +0530

    added redux-persist to persist the cart and categories state

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <Provider loading={null} store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
